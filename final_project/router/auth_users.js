781b6f180ad23962a58a424c604dc2a178aca744
JotaJara <jota.jara354@gmail.com>
2024-05-06 20:00:25 -0400

    -

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -35,13 +35,13 @@ regd_users.post("/login", (req,res) => {
       return res.status(404).json({message: "Error logging in"});
   }
  if (authenticatedUser(username,password)) {
-    let accessToken = jwt.sign({
+    const accessToken = jwt.sign({
       data: password
     }, 'access', { expiresIn: 60 * 60 });
     req.session.authorization = {
       accessToken,username
   }
-  return res.status(200).send("User successfully logged in");
+  return res.status(200).send("User successfully logged in with this token: " + accessToken);
   } else {
     return res.status(208).json({message: "Invalid Login. Check username and password"});
   }
@@ -51,6 +51,22 @@ regd_users.post("/login", (req,res) => {
 regd_users.put("/auth/review/:isbn", (req, res) => {
   const isbn = req.params.isbn;
   const review = req.body.review;
+  if(req.session.authorization) {
+    token = req.session.authorization['accessToken'];
+    jwt.verify(token, "access",(err,user)=>{
+        if(!err){
+            req.user = user;
+            next();
+        }
+        else{
+            return res.status(403).json({message: "User not authenticated"})
+        }
+     });
+ } else {
+     return res.status(403).json({message: "User not logged in"})
+ }
+
+ 
 
 });
 
